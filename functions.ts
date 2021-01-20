1525bf124f1903f0d0b1d6cc485db5c2f6779876
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-01-20 16:41:25 +0100

    ic

---
+++ functions.ts
@@ -0,0 +1,37 @@
+// function statement
+function getLincerossa(a: string) : string{
+  return `${a} + rossa`
+}
+
+// function expression
+const lincerossa : (a:string) => string = (a) => {
+  return `${a} + rossa`
+}
+
+const lincerossa2 = (a: string) : string => {
+  return `${a} + rossa`
+} 
+
+
+// promise 
+const postpone = async(ms:number) : Promise<void> =>  {
+
+  await new Promise((resolve): void => {
+    setTimeout(() => {
+      resolve(true)
+    }, ms)
+  })
+}
+
+type Data = {
+  value: string
+} 
+
+
+const getData : (fixtures: Array<Data>) => Promise<Data[]> = async (fixtures) => {
+  await postpone(500)
+  return fixtures
+}
+
+const fixtures = [{value: "1123"}]
+const calendars : Promise<Array<Data>> = getData(fixtures)
