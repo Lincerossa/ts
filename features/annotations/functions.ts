f1976a6cc5597ecedb30da2ccab8234e7cd34f9a
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-01-25 12:05:59 +0100

    ✏️ annotation -> function

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -4,3 +4,12 @@ const add : (a:number, b:number) => number = (a, b) => {
   return a + b
 
 }
+
+const neverFunction : (message: string) => never = (message: string) => {
+  throw new Error(message)
+}
+
+
+const never : (a: string,  b: Array<string>) => void = (a, b) => {
+  console.log({a,b})
+}
