dce866629f13c80f0fa55e292736765a2997922e
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-01-26 16:09:15 +0100

    📖 classes in ts (private, protected and public) + constructor shortcut

---
+++ features/classes.ts
@@ -0,0 +1,94 @@
+// FUNCTIONS
+
+class Car {
+  drive(): void {
+    console.log("brum brum")
+  }
+} 
+
+const peugeut = new Car()
+
+peugeut.drive()
+
+class AmazingCar extends Car {
+  speed(): number {
+    return 212
+  }
+}
+
+const swiffer = new AmazingCar()
+console.log(swiffer.speed())
+
+
+// public
+class House {
+
+  public getPrice(): number {
+    return 1200
+  }
+}
+
+const myHouse = new House()
+myHouse.getPrice()
+// private
+
+class HouseChild extends House {
+  private getPriceChild(): number {
+    return 1200
+  }
+}
+
+const myHouseChild = new HouseChild()
+// ⛔️ myHouseChild.getPriceChild()
+myHouseChild.getPrice()
+
+// protected
+class Scooter {
+  protected getScooterPrice(): number {
+    return 1200
+  }
+
+}
+
+class Gilera extends Scooter {
+
+  public getFinalPrice(): number {
+    return this.getScooterPrice() + 100
+  }
+}
+
+const myGilera = new Gilera()
+console.log("my gilera price:", myGilera.getFinalPrice())
+
+
+
+
+
+// PROTERTY
+
+class Supercar {
+  color: string = "white"
+  public getPrice(): number {
+    return 100
+  }
+}
+
+// BAD
+class CustomSupercar {
+  color : string
+  constructor(color: string) {
+    this.color = color
+  }
+  public getPrice(): number {
+    return 100
+  }
+}
+
+// NICE
+class CustomSupercar2 {
+  constructor(public color: string) {
+  }
+  public getPrice(): number {
+    return 100
+  }
+}
